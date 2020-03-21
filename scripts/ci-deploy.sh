c17025dfdb6ac416e8a740aa3f62a43f2bbe116d
Ian <Ian@ianleedham.dev>
2020-03-21 17:39:02 +0000

    adding circle ci config

---
+++ scripts/ci-deploy.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+# exit script when any command ran here returns with non-zero exit code
+set -e
+
+COMMIT_SHA1=$CIRCLE_SHA1
+
+# We must export it so it's available for envsubst
+export COMMIT_SHA1=$COMMIT_SHA1
+
+# since the only way for envsubst to work on files is using input/output redirection,
+#  it's not possible to do in-place substitution, so we need to save the output to another file
+#  and overwrite the original with that one.
+# envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
+# mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
+
+echo "$DOCKER_KEY" | base64 --decode > key.pem
+echo "$DOCKER_CERT" | base64 --decode > cert.pem
+echo "$DOCKER_CA" | base64 --decode > ca.pem
+
+docker \
+  -H=$DOCKER_SERVER \
+  --tlscacert=ca.pem \
+  --tlskey=key.pem \
+  --tlscert=cert.pem \
+  --tlsverify
+  info
+
+echo $IMAGE_NAME:$CIRCLE_SHA1
